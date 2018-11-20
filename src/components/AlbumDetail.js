e7d1d427bc97079dc3f7320db34b3d7b0d45d0d8
swojeet <swojeetkayastha@gmail.com>
2018-11-20 14:46:24 +0545

    Stylize Buy now button and open Amazon in browser after button is pressed

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-   const { title, artist, thumbnail_image, image } = album;
+   const { title, artist, thumbnail_image, image, url } = album;
    const {
      headerContentStyle,
      thumbnailStyle,
@@ -33,6 +34,12 @@ const AlbumDetail = ({ album }) => {
             source={{ uri: image }}
           />
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => Linking.openURL(url)}>
+            Buy Now
+          </Button>
+        </CardSection>
       </Card>
     );
 };
@@ -40,7 +47,8 @@ const AlbumDetail = ({ album }) => {
 const styles = {
   headerContentStyle: {
     flexDirection: 'column',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    marginLeft: 5
   },
   headerTextStyle: {
     fontSize: 18
